5b645d0623afbc6c5f209e50393f49d6b97feaa2
charter <charter43@gmail.com>
2022-10-26 11:57:00 +0700

    .

--- src/main/java/com/backend/dtsrea4b/fin/application/Application.java
+++ src/main/java/com/backend/dtsrea4b/fin/application/Application.java
@@ -1,9 +1,14 @@
 package com.backend.dtsrea4b.fin.application;
 
+import java.util.Arrays;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,7 +26,21 @@ public class Application {
 			}
 			
 		};
+		
 	}
+	
+	 @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	        final CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mercury-news.netlify.app" , "https://6358b52b4c7fdb49de4df3fe--gilded-baklava-466c79.netlify.app"));
+	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+	        configuration.setAllowCredentials(true);
+	        configuration.setAllowedHeaders(Arrays.asList("*"));
+	        configuration.setExposedHeaders(Arrays.asList("x-auth-token", "xsrf-token"));
+	        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
